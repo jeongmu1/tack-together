6ec0e01cc61a8266a17efec737e6e008c9e87e6b
Kang <Kang815595@gmail.com>
2023-03-22 13:30:32 +0900

    Initial commit

---
+++ app/src/main/java/com/dnlab/tack_together/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dnlab.tack_together;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AsyncTask
+    }
+}
