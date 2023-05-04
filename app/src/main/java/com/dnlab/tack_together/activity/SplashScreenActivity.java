c7b7b4e7bbb0adba12d6dd892d314f0e8595ea17
miru <alfn051@gmail.com>
2023-05-04 22:32:11 +0900

    feat: 스플래시 스크린 추가

---
+++ app/src/main/java/com/dnlab/tack_together/activity/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.dnlab.tack_together.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dnlab.tack_together.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    private static int SPLASH_SCREEN_TIMEOUT = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
