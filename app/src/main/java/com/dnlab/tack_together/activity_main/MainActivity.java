ab86bdd507b7dd29f7ca34bfdd1f6d5c377f75a2
miru <alfn051@gmail.com>
2023-06-01 22:52:26 +0900

    feat: 동승중 화면, 정산화면 액티비티 생성 및 레이아웃 제작

--- app/src/main/java/com/dnlab/tack_together/activity_main/MainActivity.java
+++ app/src/main/java/com/dnlab/tack_together/activity_main/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dnlab.tack_together.activity_main;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -24,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
+        }
     }
 }
