8557d8eba1e981333a064814bb89f39dde3fab81
Kang <Kang815595@gmail.com>
2023-06-03 19:42:37 +0900

    fix: 위치공유 액티비티가 종료되어도 계속해서 서버로 위치상태를 보내는 문제 해결

--- app/src/main/java/com/dnlab/tack_together/activity_match/MatchLocationSharingActivity.java
+++ app/src/main/java/com/dnlab/tack_together/activity_match/MatchLocationSharingActivity.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.res.ColorStateList;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
