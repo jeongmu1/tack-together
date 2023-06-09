75136d37f5a6d31871ea91570dd1d44fb7bd171d
miru <alfn051@gmail.com>
2023-06-09 18:19:26 +0900

    feat: 내정보 액티비티 생성

---
+++ app/src/main/java/com/dnlab/tack_together/activity_main/MyInfoActivity.java
@@ -0,0 +1,12 @@
+package com.dnlab.tack_together.activity_main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
