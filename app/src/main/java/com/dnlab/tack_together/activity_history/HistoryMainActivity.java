8fc993cf47cfecc9b6668d39bd8d7cb9937d9951
miru <alfn051@gmail.com>
2023-06-11 16:45:17 +0900

    feat: 이용기록 기능 추가중

--- app/src/main/java/com/dnlab/tack_together/activity_history/HistoryMainActivity.java
+++ app/src/main/java/com/dnlab/tack_together/activity_history/HistoryMainActivity.java
@@ -6,9 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.dnlab.tack_together.R;
 import com.dnlab.tack_together.adapter_history.HistoryAdapter;
+import com.dnlab.tack_together.api.dto.history.HistorySummaryDTO;
 import com.dnlab.tack_together.api.dto.history.HistorySummaryListDTO;
 import com.dnlab.tack_together.databinding.ActivityHistoryMainBinding;
 import com.dnlab.tack_together.retrofit.HistoryAPI;
@@ -23,32 +27,80 @@ import retrofit2.Response;
 
 public class HistoryMainActivity extends AppCompatActivity {
 
-    private ActivityHistoryMainBinding binding;
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
+//    private ActivityHistoryMainBinding binding;
+//    private RecyclerView recyclerView;
+//    private RecyclerView.Adapter mAdapter;
+//    private RecyclerView.LayoutManager layoutManager;
+
+    private ScrollView historyScrollView;
+    private LinearLayout historyItemsLayout;
+
+
     private HistoryAPI historyAPI;
     private static final String TAG = "HistoryMainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_main);
+
+        historyScrollView = findViewById(R.id.historyScrollView);
+        historyItemsLayout = findViewById(R.id.historyItemsLayout);
 
         historyAPI = RetrofitBuilder.getInstance(getApplicationContext()).getRetrofit().create(HistoryAPI.class);
 
-        binding = ActivityHistoryMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-        setContentView(R.layout.activity_history_main);
 
-        RecyclerView recyclerView = findViewById(R.id.history_summary_items);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager((Context) this);
-        recyclerView.setLayoutManager(linearLayoutManager); //LayoutManager 설정
-
-        HistoryAdapter historyAdapter = new HistoryAdapter(fetchHistorySummaries());
-        recyclerView.setAdapter(historyAdapter); //Adapter 설정
+//        binding = ActivityHistoryMainBinding.inflate(getLayoutInflater());
+////        setContentView(binding.getRoot());
+//
+//
+////        RecyclerView recyclerView = findViewById(R.id.history_summary_items);
+//
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager((Context) this);
+//        recyclerView.setLayoutManager(linearLayoutManager); //LayoutManager 설정
+//
+//        HistoryAdapter historyAdapter = new HistoryAdapter(fetchHistorySummaries());
+//        recyclerView.setAdapter(historyAdapter); //Adapter 설정
 
     }
+
+    private LinearLayout createItemLayout(HistorySummaryDTO item){
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        mainLayout.setLayoutParams(params);
+
+        LinearLayout topLayout = new LinearLayout(this);
+        topLayout.setOrientation(LinearLayout.HORIZONTAL);
+        topLayout.setLayoutParams(params);
+
+        LinearLayout middleLayout = new LinearLayout(this);
+        middleLayout.setOrientation(LinearLayout.HORIZONTAL);
+        middleLayout.setLayoutParams(params);
+
+        LinearLayout bottomLayout = new LinearLayout(this);
+        bottomLayout.setOrientation(LinearLayout.HORIZONTAL);
+        bottomLayout.setLayoutParams(params);
+
+        mainLayout.addView(topLayout);
+        mainLayout.addView(middleLayout);
+        mainLayout.addView(bottomLayout);
+
+        TextView dateText = new TextView(this);
+        dateText.setLayoutParams(params);
+
+        TextView paymentText = new TextView(this);
+        paymentText.setLayoutParams(params);
+
+        topLayout.addView(dateText);
+        topLayout.addView(paymentText);
+
+        return mainLayout;
+    }
+
     private List<HistorySummaryListDTO> fetchHistorySummaries() {
         List<HistorySummaryListDTO> historyData = new ArrayList<>();
 
